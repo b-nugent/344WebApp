1e178b76df56394122433275550eb56d2c67b2ff
Andrew Popovich <andrewpop1118@hotmail.com>
2015-10-17 22:03:17 -0400

    Added basic database scripts

---
+++ Database Scripts/DropTablesScript.sql
@@ -0,0 +1,5 @@
+DROP TABLE [aspnet-WebApplication5-20150927053851].dbo.UserChatHistory;
+DROP TABLE [aspnet-WebApplication5-20150927053851].dbo.ChatHistory;
+DROP TABLE [aspnet-WebApplication5-20150927053851].dbo.StockNotes;
+DROP TABLE [aspnet-WebApplication5-20150927053851].dbo.StockTransactions;
+DROP TABLE [aspnet-WebApplication5-20150927053851].dbo.CalendarEvents;
