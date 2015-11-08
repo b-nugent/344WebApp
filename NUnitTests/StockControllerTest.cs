50d8c39aa91883fd638ca66cc59885b972059ff0
Damas <dam6660@rit.edu>
2015-11-08 13:55:39 -0500

    New NUnit project added to solution

---
+++ NUnitTests/StockControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class StockControllerTest
+    {
+        [Test]
+        public void TestGetStockName()
+        {
+            string result = StockInfo.StockHandler.makeString("goog");
+            Assert.AreEqual(result, "http://finance.yahoo.com/d/quotes.csv?s=GOOG&f=snahgkj");
+        }
+        
+    }
+}
