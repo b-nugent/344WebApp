22f6c2e47cdda1df114a73f10219366da276094c
Brian Nugent <b.nugent@outlook.com>
2015-11-08 16:27:52 -0500

    Refactored and moved the calendar page so that it is not a part of the home folder. Also made sure the nav link works in layout

--- WebProject/Controllers/CalendarController.cs
+++ WebProject/Controllers/CalendarController.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication5.App_Data;
 
-namespace WebApplication5.Controllers
-{
-    public class CalendarController : Controller
-    {
+namespace WebApplication5.Controllers {
+    public class CalendarController : Controller {
         //
         // GET: /Calendar/
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
+            ViewBag.Message = "Calendar";
             return View();
         }
 
-        public ActionResult storeEvent() {
+        public ActionResult StoreEvent(string EventName, string EventDescription, DateTime EventStart, DateTime EventEnd) {
+            MySqlConnection db = new MySqlConnection();
+            db.CreateConn();
             
-            return View();
+            return RedirectToAction("Index");
         }
 	}
 }
