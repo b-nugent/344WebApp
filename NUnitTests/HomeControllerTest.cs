9d72febd36229d6a6b96f8fd26aa5dd8dc35a37a
Damas <dam6660@rit.edu>
2015-11-08 16:26:09 -0500

    Added initial HomeController Tests

---
+++ NUnitTests/HomeControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Web.Mvc;
+using System.Web;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    class HomeControllerTest
+    {
+        [Test]
+        public void TestAboutIndex()
+        {
+            var obj = new WebApplication5.Controllers.HomeController();
+            var actionResult = obj.About() as ViewResult;
+            Assert.That(actionResult.ViewName, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void TestUpdateStatus()
+        {
+            var obj = new WebApplication5.Controllers.HomeController();
+            var result = (RedirectToRouteResult)obj.UpdateStatus("Test Status");
+            Assert.That(result.RouteValues["action"], Is.EqualTo("Index"));
+        }
+
+    }
+}
