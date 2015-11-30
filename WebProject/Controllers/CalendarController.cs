693b462e747802c6e85df5916a760cbfc7caa3c3
Brian Nugent <b.nugent@outlook.com>
2015-11-30 17:44:52 -0500

    Added in the functionality to do stored procedures for the add event function. Based it on the stock note function. Just need the procedure in the database.

--- WebProject/Controllers/CalendarController.cs
+++ WebProject/Controllers/CalendarController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication5.App_Data;
+using System.Data.SqlClient;
 
 namespace WebApplication5.Controllers {
     public class CalendarController : Controller {
@@ -15,9 +16,22 @@ namespace WebApplication5.Controllers {
         }
 
         public ActionResult StoreEvent(string EventName, string EventDescription, DateTime EventStart, DateTime EventEnd) {
+            string UserID = User.Identity.GetUserId();
             MySqlConnection db = new MySqlConnection();
             db.CreateConn();
-            
+
+            SqlCommand cmd = new SqlCommand("AddEvent", db.Connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@UserId", UserID));
+            cmd.Parameters.Add(new SqlParameter("@EventName", EventName));
+            cmd.Parameters.Add(new SqlParameter("@EventDescription", EventDescription));
+            cmd.Parameters.Add(new SqlParameter("@EventStart", EventStart));
+            cmd.Parameters.Add(new SqlParameter("@EventEnd", EventEnd));
+
+            db.Command = cmd;
+            db.Command.Prepare();
+            db.Command.ExecuteNonQuery();
+
             return RedirectToAction("Index");
         }
 	}
