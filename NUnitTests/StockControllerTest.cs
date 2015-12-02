21c2ee030261df9c009c87d3186cd533f3369273
Damas <dam6660@rit.edu>
2015-12-02 15:29:43 -0500

    Test adding a stock note.

--- NUnitTests/StockControllerTest.cs
+++ NUnitTests/StockControllerTest.cs
@@ -20,6 +20,20 @@ namespace NUnitTests
             var result = controller.IndividualStock() as ViewResult;
             Assert.AreEqual(expected, result.ViewName);
         }
+
+        [Test]
+        public void TestAddNote()
+        {
+            var stock = "GOOG";
+            var stockNote = "My favorite stocks";
+
+            var controller = new WebApplication5.Controllers.StockController();
+            var result = controller.AddNote(stock, stockNote) as RedirectToRouteResult;
+
+            Assert.AreEqual("Index", result.RouteValues["Action"]);
+            //Assert.NotNull(result, "Not a redirect result");
+
+        }
         
     }
 }
