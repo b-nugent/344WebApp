962fb589c84064f71278e34597016c026d4770d3
Andrew Popovich <andrewpop1118@hotmail.com>
2015-12-02 14:32:35 -0500

    Created GetCalendarEvents stored proc

---
+++ Database Scripts/Stored Procedures/CreateGetCalendarEventsSP.sql
@@ -0,0 +1,22 @@
+USE [aspnet-WebApplication5-20150927053851]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Andrew Popovich
+-- Create date: 12-2-15
+-- Description:	Return the events for a user
+-- =============================================
+CREATE PROCEDURE [dbo].[GetCalendarEvents]
+	@UserId nvarchar(128)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT EventName, EventStartTime, EventEndTime, EventDescription
+    FROM dbo.CalendarEvents
+    WHERE (UserID = @UserId);
+END
