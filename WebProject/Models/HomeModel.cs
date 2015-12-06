3c3ee7714b57f2e4429e0fc7a066f303b258a7f8
Andrew Popovich <andrewpop1118@hotmail.com>
2015-12-06 14:03:52 -0500

    Added Events to Main Page

---
+++ WebProject/Models/HomeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class HomeModel
+    {
+        public UserPostsModels Posts { get; set; }
+
+        public List<EventModel> Events { get; set; }
+
+    }
+}
